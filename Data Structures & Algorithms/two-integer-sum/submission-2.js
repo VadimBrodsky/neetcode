775b47817137f80c9b6ccc6a248b22eddd8b79d5
Vadim Brodsky <VadimBrodsky@users.noreply.github.com>
2026-07-21 19:08:58 +0200

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const hash = nums.reduce((acc, num, i) => {
+            acc[num] = i;
+            return acc;
+        }, {});
+
+        for (let num in hash) {
+            const complement = Math.abs(target - num);
+            if (hash[complement]) {
+                const small = Math.min(hash[num], hash[complement]);
+                const large = Math.max(hash[num], hash[complement]);
+                return [small, large];
+            }
+        }
+
+        return [];
+    }
+}
